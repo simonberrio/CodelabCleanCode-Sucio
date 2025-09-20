20e16b9a0c4ad7f893583278d7f5d0815f268e78
Altos01 <logistica@altos-soft.com>
2025-09-20 08:58:53 -0500

    Fisrt cOMMIT

---
+++ app/src/main/java/co/edu/udea/compumovil/gr06_20252/lab1/MainActivity.kt
@@ -0,0 +1,47 @@
+package co.edu.udea.compumovil.gr06_20252.lab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr06_20252.lab1.ui.theme.Labs20252Gr06Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Labs20252Gr06Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Labs20252Gr06Theme {
+        Greeting("Android")
+    }
+}
