8b7216480f62b44bfab719f9bcb3f59df23fa6d3
Altos01 <logistica@altos-soft.com>
2025-09-24 23:26:04 -0500

    Configuración inicial del proyecto

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20252/lab1/PersonalDataActivity.kt
@@ -0,0 +1,47 @@
+package co.edu.udea.compumovil.gr06_20252.lab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr06_20252.lab1.ui.theme.Labs20252Gr06Theme
+
+class PersonalDataActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Labs20252Gr06Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    Labs20252Gr06Theme {
+        Greeting2("Android")
+    }
+}
---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20252/lab1/ui/PersonalDataViewModel.kt
@@ -0,0 +1,38 @@
+package co.edu.udea.compumovil.gr06_20252.lab1.ui
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class PersonalFormState(
+    val firstName: String = "",
+    val lastName: String = "",
+    val gender: String = "", // "M", "F", ""
+    val birthDateMillis: Long? = null,
+    val education: String = ""
+)
+
+class PersonalDataViewModel(private val state: SavedStateHandle) : ViewModel() {
+    companion object {
+        private const val KEY_STATE = "personal_state"
+    }
+
+    private val _uiState = MutableStateFlow(
+        state.get<PersonalFormState>(KEY_STATE) ?: PersonalFormState()
+    )
+    val uiState: StateFlow<PersonalFormState> = _uiState.asStateFlow()
+
+    fun updateFirstName(value: String) { update { it.copy(firstName = value) } }
+    fun updateLastName(value: String) { update { it.copy(lastName = value) } }
+    fun updateGender(value: String) { update { it.copy(gender = value) } }
+//    fun updateBirthDate(millis: Long?) { update { it.copy(birthDateMillis = millis) } }
+    fun updateEducation(value: String) { update { it.copy(education = value) } }
+
+    private fun update(transform: (PersonalFormState) -> PersonalFormState) {
+        val newState = transform(_uiState.value)
+        _uiState.value = newState
+        state[KEY_STATE] = newState
+    }
+}
